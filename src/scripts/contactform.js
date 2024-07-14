60727c869f39df721c42dedd48cb2c3cf01a59f4
Manish Parkar <parkarmanish@manishparkar.com>
2024-07-14 17:23:23 +0530

    Create contactform.js

---
+++ src/scripts/contactform.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import './ContactForm.css';
+
+function ContactForm() {
+  return (
+    <section className="contact-form">
+      <h2>Contact Me</h2>
+      <form>
+        <label>
+          Name:
+          <input type="text" name="name" />
+        </label>
+        <label>
+          Email:
+          <input type="email" name="email" />
+        </label>
+        <label>
+          Message:
+          <textarea name="message"></textarea>
+        </label>
+        <button type="submit">Send</button>
+      </form>
+    </section>
+  );
+}
+
+export default ContactForm;
